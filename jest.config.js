08e06eacf028ecc23db102ad19ef00654fbff1b5
Tomi Soininen <tomisoinine@gmail.com>
2026-03-29 22:23:48 +0300

    Enhance Jest config with coverage options
    Added coverage configuration to Jest settings.

--- jest.config.js
+++ jest.config.js
@@ -2,3 +2,16 @@ export default {
   testEnvironment: "node",
   transform: {},
 };
+export default {
+  testEnvironment: "node",
+  transform: {},
+
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/.internal/"
+  ],
+
+  collectCoverageFrom: [
+    "src/**/*.js"
+  ]
+};
